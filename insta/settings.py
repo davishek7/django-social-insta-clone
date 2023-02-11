1cf45fee640047b7a13c8b76aa3ea05d08a866b2
Avishek Das <davishek7@gmail.com>
2023-02-11 10:54:57 +0530

    added check for working dir app

--- insta/settings.py
+++ insta/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getcwd() == '/app' else True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
 
